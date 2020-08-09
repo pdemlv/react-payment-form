5f226a0c258aa9a8a141bf1bf9f20361e5328e07
Pavlo Demydiuk <pavlod@kyros.ai>
2020-08-09 22:37:59 +0300

    Updates

---
+++ precache-manifest.28c4f003b4a3f6cff5b67d9843b98b4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a739be9963ace455640759d180fa199",
+    "url": "/react-payment-form/index.html"
+  },
+  {
+    "revision": "d5a7eb6640c0667835bf",
+    "url": "/react-payment-form/static/css/main.8a943fb0.chunk.css"
+  },
+  {
+    "revision": "3363473e518c9ea6bfd7",
+    "url": "/react-payment-form/static/js/2.9bb1660f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-payment-form/static/js/2.9bb1660f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5a7eb6640c0667835bf",
+    "url": "/react-payment-form/static/js/main.a45a6e0d.chunk.js"
+  },
+  {
+    "revision": "bf90d5f15117d74441a7",
+    "url": "/react-payment-form/static/js/runtime-main.de387c20.js"
+  }
+]);
